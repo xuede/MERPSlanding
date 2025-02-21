cda3a71d50de4af937b537d7a48ae91bf7d005f9
Michael Wargula <mdub@i3ex.org>
2025-02-21 02:47:32 +0000

    Update setup script: replace npm with yarn for global package installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 
 ##Comment this line for DEV, this is STATIC PROD the following lines to install yarn and serve PRODUCTION
 #npm install && npm run build
-npm install -g #&& serve -s build
+yarn global add serve && npm install -g #&& serve -s build
 
 
 ##Uncomment the following lines to install yarn and serve DEVELOPMENT
