6d243900d4511b4e2459ae6ac6b1c9643cc481f2
Michael Wargula <mdub@i3ex.org>
2025-02-21 03:50:54 +0000

    adjusting for deployment

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,10 @@
 # bin/bash
+#
+yarn global add serve
+##COMMENT 👇 this line for DEV, this is STATIC PROD the following lines to install yarn and serve PRODUCTION
+npm install -g #&& serve -s build #for vps bare metal etc.
 
-##Comment this line for DEV, this is STATIC PROD the following lines to install yarn and serve PRODUCTION
 npm install -g && npm run build #for fleek
-#npm install -g #&& serve -s build #for vps bare metal etc.
-
 ##Uncomment the following lines to install yarn and serve DEVELOPMENT
 #yarn global add serve && yarn start
 #sudo chown -R $USER:$USER /workspaces/*
