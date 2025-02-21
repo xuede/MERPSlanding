f900fb0d388c66d99062d9a38dc609785a9d92d3
Michael Wargula <mdub@i3ex.org>
2025-02-21 04:31:02 +0000

    Update devcontainer configuration: simplify postCreateCommand and comment out yarn global installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,10 @@
 # bin/bash
 #
-yarn global add serve
+#yarn global add serve
 ##COMMENT 👇 this line for DEV, this is STATIC PROD the following lines to install yarn and serve PRODUCTION
 #npm install -g #&& serve -s build #for vps bare metal etc.
-
-npm install -g && npm run build #for fleek
+npm add serve && npm install -g && npm run build
+#npm install -g && npm run build #for fleek
 ##Uncomment the following lines to install yarn and serve DEVELOPMENT
 #yarn global add serve && yarn start
 #sudo chown -R $USER:$USER /workspaces/*
