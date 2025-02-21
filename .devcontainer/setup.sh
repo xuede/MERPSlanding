e495f34fce7abcaf31233437f49d7dd2d66a6c1d
Michael Wargula <mdub@i3ex.org>
2025-02-21 01:40:22 +0000

    Refactor project structure: remove unused files, add README, robots.txt, and environment example; implement initial App and testing setup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+# bin/bash
+
+##Uncomment the following lines to install yarn and serve PRODUCTION
+yarn global add serve #&& serve -s build
+
+
+##Uncomment the following lines to install yarn and serve DEVELOPMENT
+#yarn global add serve && yarn start
+#sudo chown -R $USER:$USER /workspaces/*
+#yarn start
