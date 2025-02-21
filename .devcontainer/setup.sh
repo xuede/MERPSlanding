7332996affd85b0100e2a963d78b1b2d6e898138
Michael Wargula <mdub@i3ex.org>
2025-02-21 03:44:53 +0000

    ``` No code changes made. ```

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,8 @@
 # bin/bash
 
 ##Comment this line for DEV, this is STATIC PROD the following lines to install yarn and serve PRODUCTION
-npm install -g && npm run build
-#npm install -g #&& serve -s build
-
+npm install -g && npm run build #for fleek
+#npm install -g #&& serve -s build #for vps bare metal etc.
 
 ##Uncomment the following lines to install yarn and serve DEVELOPMENT
 #yarn global add serve && yarn start
